6dead8d12d936e6df725be555d5729a4e0c69b9a
Jessica Karpovich <karpovichjm@gmail.com>
2018-01-19 13:49:27 -0800

    Added margins to order summary.

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ const color1 = document.querySelector("#color_1");
 const color2 = document.querySelector("#color_2");
 const image = document.querySelector(".js-product");
 
+
+// Change image of product based on user selected color
 function changeColor(color) {
     if (color == color1 && image.getAttribute('src') == "images/star-wars-2369316_640.jpg") {
         image.src = "images/star-wars-2908139_640.jpg";
