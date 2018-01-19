9816c82444d7a8185374d7e14713cc5cb7d8eb76
Jessica Karpovich <karpovichjm@gmail.com>
2018-01-19 13:28:53 -0800

    Finished radio button styling and event handling.

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const color1 = document.querySelector("#color_1");
+const color2 = document.querySelector("#color_2");
+const image = document.querySelector(".js-product");
+
+function changeColor(color) {
+    if (color == color1 && image.getAttribute('src') == "images/star-wars-2369316_640.jpg") {
+        image.src = "images/star-wars-2908139_640.jpg";
+    }
+    else if (color == color2 && image.getAttribute('src') == "images/star-wars-2908139_640.jpg") {
+        image.src = "images/star-wars-2369316_640.jpg"
+    }
+}
+
+color1.addEventListener('click', function() { changeColor(color1) }, false);
+color2.addEventListener('click', () => { changeColor(color2) }, false);
